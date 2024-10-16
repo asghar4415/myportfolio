e5207156a9b752535f181a5c6b6dc361aa72ac7a
Asghar Ali <asgharali.224415@gmail.com>
2024-10-16 15:32:22 +0500

    updated

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true, 
+  },
+  optimizeFonts: false,
+  images: {
+    domains: [
+      "user-images.githubusercontent.com",
+      "cdn.hashnode.com",
+      "github.com"
+    ],
+  },
+};
+
+module.exports = nextConfig;
