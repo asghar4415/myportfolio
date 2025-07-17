d2d686010b0969963c95a437daf8248885fd07fc
Asghar Ali <asgharali.224415@gmail.com>
2025-07-17 17:34:30 +0500

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,75 @@
-import "./globals.css";
-import { ReactNode } from "react";
-import type { Metadata } from "next";
-import { Syne } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
+import "../global.css";
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+import { Analytics } from "./components/analytics";
 
-const syne = Syne({
+export const metadata: Metadata = {
+  title: {
+    default: "chronark.com",
+    template: "%s | chronark.com",
+  },
+  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  openGraph: {
+    title: "chronark.com",
+    description:
+      "Co-founder of unkey.dev and founder of planetfall.io",
+    url: "https://chronark.com",
+    siteName: "chronark.com",
+    images: [
+      {
+        url: "https://chronark.com/og.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Chronark",
+    card: "summary_large_image",
+  },
+  icons: {
+    shortcut: "/favicon.png",
+  },
+};
+const inter = Inter({
   subsets: ["latin"],
-  display: "block",
-  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-inter",
 });
 
+const calSans = LocalFont({
+  src: "../public/fonts/CalSans-SemiBold.ttf",
+  variable: "--font-calsans",
+});
 
-
-type RootLayoutProps = {
-  children: ReactNode;
-};
-
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en">
+    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+      <head>
+        <Analytics />
+      </head>
       <body
-        className={`${syne.className} scroll-smooth scrollbar-thin scrollbar-track-[#0E1016] scrollbar-thumb-[#212531]`}
+        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+          }`}
       >
         {children}
-        <Analytics />
       </body>
     </html>
   );
